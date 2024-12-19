2f8096cc9ba945d1523785b93c9e15296b8850e2
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2024-12-19 06:12:14 +0200

    Remove folder and update app.js

--- 3-JS Advanced DOM and Event/app.js
+++ 3-JS Advanced DOM and Event/app.js
@@ -9,3 +9,39 @@ anchorElement.href = "https://google.com";
 
 anchorElement = document.querySelector("p a"); // p a { color: red; }
 anchorElement.href = "https://academind.com";
+
+// ADD AN ELEMENT
+// 1. Create the new element
+let newAnchorElenment = document.createElement("a");
+console.dir(newAnchorElenment);
+newAnchorElenment.href = "https://google.com";
+newAnchorElenment.textContent = "This is a new link";
+
+// 2. Get access to the parent element that should hold the new element
+let fristParagraph = document.querySelector("p");
+
+// 3. Insert the new element into the parent element content
+
+fristParagraph.append(newAnchorElenment);
+
+// REMOVE ELEMENTS
+// 1. Select the element that should be removed
+
+let secondParagraph = document.querySelector(".main-p");
+
+// 2. Remove it!
+
+secondParagraph.remove();
+
+// MOVE ELEMENTS
+
+let H1Element = document.querySelector("h1");
+
+H1Element.parentElement.append(H1Element);
+
+// inner HTML
+
+let anotherParagraph = document.createElement("p");
+anotherParagraph.innerHTML =
+  "Hi this is <strong>Important!</strong> " + "It's very <span>important</span>";
+document.body.append(anotherParagraph);
