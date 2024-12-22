f36d8b40d4edebc114711fcacb1927ad2fdddb2e
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2024-12-22 22:25:44 +0200

    Add Alternatives with "else" and "else if And realy finsh the project

--- 4-JS Input Text Counter Project/app.js
+++ 4-JS Input Text Counter Project/app.js
@@ -6,10 +6,20 @@ function updateRemainingCharacter(event) {
   const enteredText = event.target.value;
   const enteredTextLength = enteredText.length;
   const remainingCharacter = maxAllowedChars - enteredTextLength;
+  
   remainingCharsElement.textContent = remainingCharacter;
-  if (remainingCharacter <= 10) {
+  
+  if (remainingCharacter === 0) {
+    proudctNameInputElement.classList.add("error");
+    remainingCharsElement.classList.add("error");
+  }else if (remainingCharacter <= 10) {
     proudctNameInputElement.classList.add("warning");
     remainingCharsElement.classList.add("warning");
+    proudctNameInputElement.classList.remove("error");
+    remainingCharsElement.classList.remove("error");  
+  } else {
+    proudctNameInputElement.classList.remove("warning");
+    remainingCharsElement.classList.remove("warning");
   }
 }
 
