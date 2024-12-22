9881344de93ecfa0225c6dfc42a3992ff89a77ce
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2024-12-22 23:27:29 +0200

    Conditional code

---
+++ 6-JS Conditional Coding/app.js
@@ -0,0 +1,27 @@
+const myName = "Saif Eldin";
+
+if (myName === "Saif Eldin") {
+  console.log("Hello");
+}
+
+//-------------------------
+
+let isLogedIn = false;
+
+if (isLogedIn) {
+  console.log("User is loged in");
+} else if (isLogedIn === false) {
+// you can use condition !isLogedIn
+  console.log("User is Not loged in");
+}
+
+//---------------------------------
+
+
+const interdUserName = ''; // '' and 0 = false
+
+if (interdUserName) {
+    console.log('Input is Valid!')
+}else{
+    console.log('Input is NOT Valid!')
+}
