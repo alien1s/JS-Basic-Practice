7dc00ad500c5368b9b657f26c8701dcafdf5dc42
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2024-12-22 22:09:19 +0200

    Add a conditional if statment and finish the project

--- 4-JS Input Text Counter Project/app.js
+++ 4-JS Input Text Counter Project/app.js
@@ -1,4 +1,4 @@
-const proudctNameInputElement = document.getElementById('product-name');
+const proudctNameInputElement = document.getElementById("product-name");
 const remainingCharsElement = document.getElementById("remaining-chars");
 const maxAllowedChars = proudctNameInputElement.maxLength;
 
@@ -6,8 +6,11 @@ function updateRemainingCharacter(event) {
   const enteredText = event.target.value;
   const enteredTextLength = enteredText.length;
   const remainingCharacter = maxAllowedChars - enteredTextLength;
-
   remainingCharsElement.textContent = remainingCharacter;
+  if (remainingCharacter <= 10) {
+    proudctNameInputElement.classList.add("warning");
+    remainingCharsElement.classList.add("warning");
+  }
 }
 
-proudctNameInputElement.addEventListener('input', updateRemainingCharacter);
+proudctNameInputElement.addEventListener("input", updateRemainingCharacter);
