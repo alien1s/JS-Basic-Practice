1370e29a11a9a810176712d2aa8776658631d12b
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2024-12-28 15:30:03 +0200

    calculateSumButtonElement

---
+++ 8-loops-in-action/loops-in-action.js
@@ -0,0 +1,28 @@
+
+// -----------------------calculateSumButtonElement-------------------
+
+const calculateSumButtonElement = document.querySelector("#calculator button");
+
+function calculateSum(event) {
+  const userInputNumberElement = document.getElementById("user-number");
+  const enterdNumber = userInputNumberElement.value;
+
+  let sumUpToNumber = 0;
+
+  for (let i = 0; i <= enterdNumber; i++) {
+    sumUpToNumber = sumUpToNumber + i;
+  }
+
+  const outputResultElement = document.getElementById("calculated-sum");
+  outputResultElement.textContent = sumUpToNumber;
+  outputResultElement.style.display = "block";
+}
+
+calculateSumButtonElement.addEventListener("click", calculateSum);
+
+
+// -----------------------------------------------------------------------
+
+
+
+
