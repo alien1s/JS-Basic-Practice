7a017d97f52f8eec63b722490a88ac671b3e5263
Saif Eldin Abdelrahman <saif.at.miracle@gmail.com>
2025-02-04 00:47:40 +0200

    edit my solution in ex 1

--- 2-JS Basics Exercise/exercise1.js
+++ 2-JS Basics Exercise/exercise1.js
@@ -7,20 +7,22 @@
 // 1) Create three new variables:
 //    - A variable that stores the name of an online course of your choice
 
-let courseName ='Zone';
+let courseName = "Zone";
 
 //    - A variable that stores the price of that course
 
-let coursePrice =5000 + '$';
+let coursePrice = 5000 + "$";
 
 //    - A variable that stores the three main goals that you have, when taking this course
 
-let courseGoals = ['learn', 'study', 'bulid'];
+let courseGoals = ["learn", "study", "bulid"];
 
 // 2) Output ("alert") the three variable values
-
+alert("myCourse name is:");
 alert(courseName);
+alert("myCourse price is:");
 alert(coursePrice);
+alert("myCourse goals is:");
 alert(courseGoals);
 
 // 3) Try "grouping" the three variables together and still output their values thereafter
@@ -31,12 +33,17 @@ let myCourse = {
   goals: courseGoals,
 };
 
+alert("myCourse data is:");
+alert(JSON.stringify(myCourse));
+alert("myCourse name is:");
 alert(myCourse.name);
+alert("myCourse price is:");
 alert(myCourse.price);
+alert("myCourse goals is:");
 alert(myCourse.goals);
 
 // 4) Also output the second element in your "main goals" variable
-
+alert("myCourse main goals is:");
 alert(myCourse.goals[1]);
 
 // 5) Add a custom command that does the following:
@@ -47,12 +54,15 @@ alert(myCourse.goals[1]);
 //      with ANY list of values
 //    - The custom command should provide the accessed value (i.e. the list element)
 
-function getListitems(array, number){
-  let arrayIndex = number-1;
-  let arrayElement= array[arrayIndex];
+function getListitems(array, number) {
+  let arrayIndex = number - 1;
+  let arrayElement = array[arrayIndex];
   return arrayElement;
 }
 // 6) Execute your custom command from (5) and output ("alert") the result
 
-let FristGoal = getListitems(myCourse.goals, 1);
+let FristGoal = getListitems(courseGoals, 1);
+alert(FristGoal);
+
+FristGoal = getListitems(myCourse.goals, 1);
 alert(FristGoal);
